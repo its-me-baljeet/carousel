494a04335fe9d7e959052aa618fcd11a7c5f87af
Baljeet Singh <bsbaljeetsingh9999@gmail.com>
2025-03-05 01:37:25 +0530

    created intial carousel

---
+++ script.js
@@ -0,0 +1,46 @@
+const slides = document.querySelectorAll(".slide");
+const pageNav = document.getElementById("page-nav");
+let counter = 0;
+
+const dots = [];
+slides.forEach((slide, idx) => {
+    slide.style.left = `${idx * 100}%`;
+});
+
+function createDots() {
+    slides.forEach((_, idx) => {
+        const dot = document.createElement("span");
+        dot.classList.add("page");
+        dot.addEventListener("click", () => {
+            counter = idx;
+            slideImage();
+        });
+        pageNav.appendChild(dot);
+        dots.push(dot);
+    });
+}
+createDots();
+updateDots();
+
+function updateDots() {
+    dots.forEach((dot, idx) => {
+        dot.classList.toggle("active", idx === counter);
+    });
+}
+
+function slideImage() {
+    slides.forEach(slide => {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+    updateDots();
+}
+
+function getNext() {
+    counter = (counter + 1) % slides.length;
+    slideImage();
+}
+
+function getPrev() {
+    counter = (counter - 1 + slides.length) % slides.length;
+    slideImage();
+}
